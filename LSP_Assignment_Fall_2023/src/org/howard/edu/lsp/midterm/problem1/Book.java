1e41450bd0fd8299adddaef3e55824635de00135
josephsankah <josephsankah@josephs-mbp>
2023-10-19 20:38:23 -0400

    My final submission for the lsp midterm.

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/midterm/problem1/Book.java
@@ -0,0 +1,53 @@
+package org.howard.edu.lsp.midterm.problem1;
+
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (year != book.year) return false;
+        if (!title.equals(book.title)) return false;
+        return author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + author.hashCode();
+        result = 31 * result + year;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925);
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
+
+        System.out.println(book1);
+        System.out.println(book2);
+
+        System.out.println("book1 equals book2: " + book1.equals(book2));
+    }
+}
